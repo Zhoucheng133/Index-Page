502ee6b91119b0fe2900ba0e8c7904360ff92162
zhoucheng <zhoucheng133@outlook.com>
2025-02-28 20:21:32 +0800

    完成表格展示

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,44 @@
 <template>
   <div>
-    <Button>Hello!</Button>
+    <DataTable :value="data" tableStyle="min-width: 50rem">
+      <Column field="name" header="名称"></Column>
+      <Column field="port" header="端口"></Column>
+      <Column field="webui" header="WebUI">
+        <template #body="slotProps">
+          {{ slotProps.data.webui==1 ? "❌" : "✅" }}
+        </template>
+      </Column>
+      <Column field="tip" header="备注"></Column>
+    </DataTable>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { Button } from 'primevue';
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import { hostname } from './static/env';
+import { Column, DataTable } from 'primevue';
+
+interface Data{
+  id: number,
+  name: string,
+  port: string,
+  webui: number,
+  tip: string
+}
+
+const data=ref<Data[]>([]);
+async function initData(){
+  const {data: response}=await axios.get(`${hostname}/api/list`);
+  console.log(response);
+  if(response!=null){
+    data.value=response.data;
+  }
+}
+
+
+onMounted(()=>{
+  initData();
+})
 
 </script>
